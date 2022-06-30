a0c8448f3645e6dbd9f4d4ba1ab697499a664d22
Peter Johnson <peterj1298@gmail.com>
2022-06-30 09:07:20 +0530

    merge conflict solved

--- Webapp/tailwind.config.js
+++ Webapp/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      height: {
+        100: "45.55rem",
+      },
       animation: {
         blob: "blob 10s infinite",
       },
@@ -54,6 +57,28 @@ module.exports = {
         xl: "1124px",
         "2xl": "1124px",
       },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      xll: { max: "1200px" },
+      // => @media (max-width: 1200px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      smm: { max: "415px" },
+      // => @media (max-width: 415px) { ... }
     },
   },
   plugins: [],
