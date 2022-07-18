e1e05de50589f5651bc9e83bbd30a5637588b297
Paul Joshi <pauljoshi@gmail.com>
2022-07-18 21:44:27 +0530

    🦸‍♀️ Hero page rename

--- Webapp/src/components/Hero.js
+++ Webapp/src/components/Hero.js
@@ -7,7 +7,7 @@ function Herotwo() {
   const setConsent = useConsentStore((state) => state.setConsent);
 
   return (
-    <div className="pt-40 h-screen flex items-center justify-center flex-col">
+    <div className="pt-52 h-screen flex items-center justify-center flex-col">
       <div className="relative w-full max-w-lg">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-red-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
